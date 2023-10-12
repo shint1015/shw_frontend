c173f92f13b8566c9fac1e7fb6d2bb0f83ad2491
shint1015 <53039633+shint1015@users.noreply.github.com>
2023-10-12 22:02:07 +0900

    [update]

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,23 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        base: {
+          50: '#fff9f4',
+          100: '#fff4ea',
+          200: '#ffefe0',
+          300: '#ffe8d1',
+          400: '#ffe2c6',
+          500: '#ffddbc',
+          600: '#ffd8b2',
+          700: '#ffd3a8',
+          800: '#ffce9e',
+          900: '#ffc993',
+          950: '#ffc489',
+          1000: '#ffbf7f',
+          DEFAULT: '#ffd8b2',
+        }
+      }
     },
   },
   plugins: [],
