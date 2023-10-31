e0321156c4bcc34b63c62cb5317c83430fcb7880
shint1015 <53039633+shint1015@users.noreply.github.com>
2023-10-31 23:24:04 +0900

    [update]

---
+++ src/app/housework/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Page = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-base-200">
+            housework
+        </main>
+    )
+}
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-base-200">
-      <a href="/api/auth/login">Login</a>
-    </main>
-  )
+"use client";
+const Home = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-base-200 text-black">
+            <a href="/api/auth/login">Login</a>
+        </main>
+    )
 }
+
+
+export default Home
