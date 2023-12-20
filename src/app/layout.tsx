10341495b79cd8cca45b597d73737e64b3b39d60
shint1015 <53039633+shint1015@users.noreply.github.com>
2023-12-20 19:33:11 +0900

    [update]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import Header from "@/components/common/Header";
-import "../styles/common.scss"
+import "@/styles/common.scss"
 
 const inter = Inter({ subsets: ['latin'] })
 
