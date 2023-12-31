a4e0cc8848d10cb6197aed568e0b3f684cddecfe
shint1015 <53039633+shint1015@users.noreply.github.com>
2023-12-31 13:04:46 +0900

    [update] RHF component

--- src/components/common/react-hook-form/TextField.tsx
+++ src/components/common/react-hook-form/TextField.tsx
@@ -1,32 +1,37 @@
-import {TextField} from "@mui/material";
-import {Control, Controller} from "react-hook-form";
-import {HouseworkFormValues} from "@/components/common/react-hook-form/type";
+import React from 'react';
+import { Controller, Control } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
 
-
-const RHFTextField = <T extends HouseworkFormValues>(
-    name: string,
-    validateRules: { [key: string]: any },
-    labels: { id: string, label: string },
-    control: Control<T>
-) => {
-    return (
-        <>
-            <Controller
-                name={name}
-                control={control}
-                rules={validateRules}
-                render={({field, fieldState}) => (
-                    <TextField
-                        label={labels.label}
-                        error={fieldState.invalid}
-                        helperText={fieldState.error?.message}
-                        {...field}
-                    />
-                )}
-            >
-            </Controller>
-        </>
-    )
+interface RHFTextFieldProps {
+    name: string;
+    labelName: string;
+    control: Control;
+    validateRules: {[key: string]: any}
+    defaultValue?: string;
 }
 
-export default RHFTextField;
+export const RHFTextField: React.FC<RHFTextFieldProps> = ({
+    name,
+    labelName,
+    control,
+    validateRules,
+    defaultValue = '',
+}) => {
+    return (
+        <Controller
+            name={name}
+            control={control}
+            defaultValue={defaultValue}
+            rules={validateRules}
+            render={({ field }) => (
+                <TextField
+                    {...field}
+                    label={labelName}
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                />
+            )}
+        />
+    );
+};
--- src/components/common/react-hook-form/Textarea.tsx
+++ src/components/common/react-hook-form/Textarea.tsx
@@ -1,34 +1,38 @@
-import {TextField} from "@mui/material";
-import {Controller} from "react-hook-form";
-import {HouseworkFormValues} from "@/components/common/react-hook-form/type";
+import React from 'react';
+import { TextField } from '@mui/material';
+import { Controller, Control } from 'react-hook-form';
 
-const RHFTextarea = <T extends HouseworkFormValues>(
-    name: string,
-    validateRules: { [key: string]: any },
-    labels: { id: string, label: string },
-    properties: {rows: number},
-    control: Control<T>
-) => {
-    return (
-        <>
-            <Controller
-                name={name}
-                control={control}
-                rules={validateRules}
-                render={({field, fieldState}) => (
-                    <TextField
-                        label={labels.label}
-                        error={fieldState.invalid}
-                        helperText={fieldState.error?.message}
-                        muiline
-                        rows={properties.rows ?? 4}
-                        {...field}
-                    />
-                )}
-            >
-            </Controller>
-        </>
-    )
+
+interface RHFTextareaProps {
+    name: string;
+    validateRules: { [key: string]: any };
+    labels: { id: string, label: string };
+    properties?: {rows: number};
+    control: Control;
 }
 
-export default RHFTextField;
+export const RHFTextarea: React.FC<RHFTextareaProps> = ({
+    name,
+    labels,
+    validateRules,
+    properties,
+    control,
+}) => {
+    if (properties === undefined) {
+    }
+    return (
+        <Controller
+            name={name}
+            control={control}
+            rules={validateRules}
+            render={({field, fieldState}) => (
+                <TextField
+                    label={labels.label}
+                    multiline
+                    rows={(properties && properties.rows) ?? 4}
+                    {...field}
+                />
+            )}
+        />
+    )
+}
