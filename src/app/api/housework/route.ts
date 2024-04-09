9ce138ce53568b8818fc6904adb9a6f954c43bd0
shint1015 <53039633+shint1015@users.noreply.github.com>
2024-04-09 10:21:55 +0900

    [update] grpc requestの共通化

--- src/app/api/housework/route.ts
+++ src/app/api/housework/route.ts
@@ -1,62 +1,41 @@
-import { transport } from "@/apiClient/client";
-import { createPromiseClient } from "@connectrpc/connect";
-import { NextResponse } from "next/server";
-import {Housework, HouseworkRequest, HouseworkResponse} from "@/services/housework_pb";
-import {HouseworkService} from "@/services/housework_connect";
-import {createParamsFromUrl} from "@utils/request";
-import {CommonResponse} from "@/services/common_pb";
+import { transport } from '@/apiClient/client';
+import { createPromiseClient } from '@connectrpc/connect';
+import {Housework, HouseworkRequest, HouseworkResponse} from '@/services/housework_pb';
+import {HouseworkService} from '@/services/housework_connect';
+import {createParamsFromUrl, createRequestMethodFunc} from '@utils/request';
+import {CommonResponse} from '@/services/common_pb';
 
 const client = createPromiseClient(HouseworkService, transport)
-export type HouseworkApiResponse = | { ok: true, data: HouseworkResponse | CommonResponse } | { ok: false, error: Error };
+export type HouseworkApiResponse = { ok: true, data: HouseworkResponse | CommonResponse } | { ok: false, error: Error };
 
 
 const doGrpcRequest = async (req: Request, method: string) => {
     const request = new HouseworkRequest();
     const housework = new Housework();
-    if (method === "GET" || method === "DELETE") {
+    if (method === 'GET' || method === 'DELETE') {
         const requestJson = createParamsFromUrl(req.url)
         switch (method) {
-            case "GET":
+            case 'GET':
                 request.fromJson(requestJson)
                 return client.getHousework(request);
-            case "DELETE":
+            case 'DELETE':
                 housework.fromJson(requestJson)
                 return client.deleteHousework(housework);
         }
-    } else if (method === "POST" || method === "PUT") {
+    } else if (method === 'POST' || method === 'PUT') {
         const data = await req.json();
         housework.fromJson(data)
         switch (method) {
-            case "POST":
+            case 'POST':
                 return client.createHousework(housework);
-            case "PUT":
+            case 'PUT':
                 return client.updateHousework(housework);
         }
     }
 }
-const doRequest = async (req: Request, method: string) => {
-    try {
-        const response = await doGrpcRequest(req, method);
-        return NextResponse.json({ ok: true, data: response }, {status: 200});
-    } catch (e) {
-        return NextResponse.json({ ok: false, error: e }, {status: 500});
-    }
-}
 
 
-
-const GET = async (req: Request) => {
-    return doRequest(req, "GET")
-}
-const POST = async (req: Request) => {
-    return doRequest(req, "POST")
-}
-const PUT = async (req: Request) => {
-    return doRequest(req, "PUT")
-}
-const DELETE = async (req: Request) => {
-    return doRequest(req, "DELETE")
-}
+const [GET, POST, PUT, DELETE] = createRequestMethodFunc<HouseworkApiResponse>(doGrpcRequest, ['GET', 'POST', 'PUT', 'DELETE'])
 
 export {
     GET,
--- utils/request.ts
+++ utils/request.ts
@@ -12,3 +12,48 @@ export const createParamsFromUrl = (url: string): {[key: string]: string} => {
     });
     return params;
 }
+
+/**
+ * Do a request
+ * @param req
+ * @param method
+ * @param grpcRequest
+ */
+export const doRequest = async (
+    req: Request,
+    method: string,
+    grpcRequest: (req: Request, method: string) => Promise<any>
+) => {
+    try {
+        const response = await grpcRequest(req, method);
+        return NextResponse.json({ ok: true, data: response }, {status: 200});
+    } catch (e) {
+        return NextResponse.json({ ok: false, error: e }, {status: 500});
+    }
+}
+
+
+/**
+ * Create a request method function
+ * @param grpcRequest
+ * @param methods
+ */
+export const createRequestMethodFunc = <ResponseType>(grpcRequest: (req: Request, method: string) => Promise<any>, methods: string[]): ResponseType[] => {
+    let requestMethodFunc = new Map<string, (req: Request) => Promise<ResponseType>>();
+    switch (methods.length) {
+        case 'GET':
+            requestMethodFunc.append(doRequest(req, "GET", grpcRequest))
+            break;
+        case 'POST':
+            requestMethodFunc.append(doRequest(req, "POST", grpcRequest))
+            break;
+        case 'PUT':
+            requestMethodFunc.append(doRequest(req, "PUT", grpcRequest))
+            break;
+        case 'DELETE':
+            requestMethodFunc.append(doRequest(req, "DELETE", grpcRequest))
+            break;
+    }
+    return requestMethodFunc;
+}
+
