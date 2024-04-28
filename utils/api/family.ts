320ed9d8ecb32c88a07a7023ff6612a49bbf142d
shint1015 <53039633+shint1015@users.noreply.github.com>
2024-04-28 23:34:18 +0900

    [update] api

--- utils/api/family.ts
+++ utils/api/family.ts
@@ -1,13 +1,14 @@
 import Common, {endpoint} from "@utils/api/common";
 
+const defaultCacheType = 'no-store';
 
 class Family extends Common {
-    static async getRole(familyId: string, cacheType: string = 'force-cache') {
+    static async getRole(familyId: string, cacheType: string = defaultCacheType) {
         const res = await fetch(`${endpoint}/family/role?familyId=${familyId}`, {cache: cacheType});
         return await res.json();
     }
 
-    static async getBelongToUser(familyId: string, cacheType: string = 'no-store') {
+    static async getBelongToUser(familyId: string, cacheType: string = defaultCacheType) {
         const res = await fetch(`${endpoint}/family/belongs_to_user?familyId=${familyId}`,{cache: cacheType});
         return await res.json();
     }
@@ -18,6 +19,19 @@ class Family extends Common {
             let resArr: Record<number, string> = [];
             for (const user of data.data.users) {
                 resArr[user.id] = user.name;
+            }
+            return resArr;
+        } else {
+            throw new Error(data.error);
+        }
+    }
+
+    static async getRoleArr (familyId: string) {
+        const data = await this.getRole(familyId);
+        if (data.ok) {
+            let resArr: Record<number, string> = [];
+            for (const role of data.data.familyRole) {
+                resArr[role.id] = role.name;
             }
             return resArr;
         } else {
