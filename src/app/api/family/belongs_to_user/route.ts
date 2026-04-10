ad02d7f112c75c2d5de09b5b1396d6573f8dcfea
Shintaro Miyata <shintaroot10@gmail.com>
2026-04-10 23:59:27 +0900

    update

--- src/app/api/family/belongs_to_user/route.ts
+++ src/app/api/family/belongs_to_user/route.ts
@@ -1,17 +1,17 @@
 import { createAuthTransport } from "@/apiClient/client";
-import { UserService, GetBelongToUserResponse, GetBelongToUserRequestSchema } from "@/gen/user_pb";
+import { UserService, ListFamilyUsersResponse, ListFamilyUsersRequestSchema } from "@/gen/user_pb";
 import { fromJson } from "@bufbuild/protobuf";
 import { createClient } from "@connectrpc/connect";
 import { createParamsFromUrl, doAuthRequest } from "@utils/request";
 
 
-export type UserApiResponse = { ok: true, data: GetBelongToUserResponse } | { ok: false, error: Error };
+export type UserApiResponse = { ok: true, data: ListFamilyUsersResponse } | { ok: false, error: Error };
 
 const doGrpcRequest = async (req: Request, _method: string, accessToken: string) => {
     const client = createClient(UserService, createAuthTransport(accessToken))
     const requestJson = createParamsFromUrl(req.url)
-    const request = fromJson(GetBelongToUserRequestSchema, requestJson);
-    return client.getBelongToUser(request);
+    const request = fromJson(ListFamilyUsersRequestSchema, requestJson);
+    return client.listFamilyUsers(request);
 }
 
 export const GET = async (req: Request) => {
